61ee62140054bc704a4676fa2954ccfcdfed81d3
Fabricio Barth <fbarth@br.ibm.com>
2015-12-05 15:05:09 -0200

    Adicionando validacao com conjunto de treino e teste.

--- scripts/textClassify/classifyQuestions.R
+++ scripts/textClassify/classifyQuestions.R
@@ -11,15 +11,42 @@ text <- removeNumbers(text)
 text <- removeWords(text, stopwords('portuguese'))
 
 # Construindo uma matriz de documentos versus termos:
-text <- stemDocument(text, language = "portuguese")
+text <- stemDocument(text, 
+                     language = "portuguese")
 myCorpus <- Corpus(VectorSource(text))
 docs_term <- DocumentTermMatrix(myCorpus)
-dataset <- as.data.frame(cbind(inspect(docs_term), questoes))
+
+dataset <- as.data.frame(
+  cbind(
+    inspect(docs_term), 
+    questoes)
+  )
 dataset$text <- NULL
+
+# separando os datasets em treino e teste
+set.seed(1234)
+ind <- sample(
+  2, 
+  nrow(dataset), 
+  replace=TRUE,
+  prob = c(0.8, 0.2))
+
+treino <- dataset[ind == 1, ]
+teste <- dataset[ind == 2, ]
 
 # criando o modelo
 library(randomForest)
-model <- randomForest(classes ~ ., data = dataset, importance = TRUE, do.trace = 100)
+model <- randomForest(
+  classes ~ ., 
+  data = treino, 
+  importance = TRUE, 
+  do.trace = 100)
 model
 plot(model)
 varImpPlot(model)
+
+preditos <- predict(model, teste)
+
+library(caret)
+confusionMatrix(preditos, teste$classes)
+
